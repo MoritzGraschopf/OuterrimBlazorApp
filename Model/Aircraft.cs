def3f74e2fd5103fcdb0f46acd49672aeadc50e3
MoG <moritz.graschopf@gmail.com>
2025-02-12 11:12:54 +0100

    added Models Part 1

---
+++ Model/Aircraft.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model;
+
+[Table("AIRCRAFTS")]
+public class Aircraft
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int Fuel { get; set; }
+    public int Speed { get; set; }
+    public int Altitude { get; set; }
+
+    [StringLength(200)]
+    public string Name { get; set; }
+}
---
+++ Model/AircraftSpezification.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model;
+
+[Table("AIRCRAFT_SPEZIFICATIONS")]
+public class AircraftSpezification
+{
+    [Key]
+    [Column("SPEZIFICATION_ID")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("STRUCTURE")]
+    public int Structure { get; set; }
+
+    [Required]
+    [Column("FUEL_TANK_CAPACITY")]
+    public int FuelTankCapacity { get; set; }
+
+    [Required]
+    [Column("MIN_SPEED")]
+    public int MinSpeed { get; set; }
+
+    [Required]
+    [Column("MAX_SPEED")]
+    public int MaxSpeed { get; set; }
+
+    [Required]
+    [Column("MAX_ALTITUDE")]
+    public int MaxAltitude { get; set; }
+
+    [StringLength(45)]
+    [Required]
+    [Column("SPEZIFICATION_CODE")]
+    public string SpezificationCode { get; set; }
+}
