0ba3c6d563e15e349d9794c07e1f81b8f35ea0bf
NickOrsolits <n.orsolits@htlkrems.at>
2025-02-12 18:16:12 +0100

    jo chef

--- Domain/Repositories/AircraftsRepository.cs
+++ Domain/Repositories/AircraftsRepository.cs
@@ -3,15 +3,9 @@ using Model.Context;
 using Model.Entities;
 
 namespace Domain.Repositories;
-public class BookRepository(OuterRimContext context) : ARepository<>(context)
+public class AircraftsRepository(OuterRimContext context) : ARepository<Aircraft>(context)
 {
-    private readonly OuterRimContext _context = context;
 
-    public override async Task<List<Aircraft>> ReadAllAsync()
-    {
-        return await _context.Set<>()
-            .Include(s => s.)
-            .ToListAsync();
-    }
+    
 }
 
