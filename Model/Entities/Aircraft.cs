29339d28db5702b59926288ab2345aa831342725
MoG <moritz.graschopf@gmail.com>
2025-02-12 12:38:42 +0100

    moved to own Folder

--- Model/Entities/Aircraft.cs
+++ Model/Entities/Aircraft.cs
@@ -7,12 +7,24 @@ namespace Model;
 public class Aircraft
 {
     [Key]
+    [Column("AIRCRAFT_ID")]
     public int Id { get; set; }
 
+    [Required]
+    [Column("FUEL")]
     public int Fuel { get; set; }
+    [Required]
+    [Column("SPEED")]
     public int Speed { get; set; }
+    [Required]
+    [Column("ALTITUDE")]
     public int Altitude { get; set; }
 
     [StringLength(200)]
+    [Required]
+    [Column("NAME")]
     public string Name { get; set; }
+
+    public int SpezificationId { get; set; }
+    public AircraftSpezification AircraftSpezification { get; set; }
 }
